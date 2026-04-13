d6b28090b70790a14744b26038a6f4c93a4aa76d
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-04-13 09:49:50 +0530

    Complete imports

---
+++ 06_chai_business/main.py
@@ -0,0 +1,13 @@
+# import recipes.flavor
+
+# print(recipes.flavor.elaichi_chai())
+
+# from recipes.flavor import elaichi_chai, ginger_chai
+
+# print(ginger_chai())
+# print(elaichi_chai())
+
+from .recipes.flavor import elaichi_chai, ginger_chai
+
+print(ginger_chai())
+print(elaichi_chai())
