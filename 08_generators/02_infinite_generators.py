dd8f2736b795fdd6fcdb89103cd99d624042cb3f
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-04-14 12:14:48 +0530

    Complete generators

---
+++ 08_generators/02_infinite_generators.py
@@ -0,0 +1,13 @@
+def infinite_chai():
+    count = 1
+    while True:
+        yield f"Refill #{count}"
+        count += 1
+
+refill = infinite_chai()
+
+for _ in range(4):
+    print(next(refill))
+
+for _ in range(6):
+    print(next(refill))
