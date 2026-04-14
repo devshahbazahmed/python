371cfb482491a74f48848b0f6d8126d4abc6c918
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-04-14 15:17:22 +0530

    Complete decorators

---
+++ exercises/07_decorators/01_caching_expensive_calculations.py
@@ -0,0 +1,19 @@
+# This function will be tested automatically.
+# Do not change the function name or parameters.
+
+
+def cache_results(func):
+    # Write your code below this line
+    cache = {}
+    def wrapper(*args):
+        if args in cache:
+            return f"From Cache: {cache[args]}"
+        else:
+            result = func(*args)
+            cache[args] = result
+            return f"Computed: {result}"
+    return wrapper
+
+@cache_results
+def multiply(a: int, b: int) -> int:
+    return a * b
